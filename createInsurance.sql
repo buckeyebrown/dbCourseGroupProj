a6517639484d90d5f19cf10a4e34a3b42fc7992f
Dylan Brown <Dylan Brown>
2017-03-09 13:45:10 -0500

    forgot commas

--- createInsurance.sql
+++ createInsurance.sql
@@ -2,11 +2,11 @@ use insurance;
 
 CREATE TABLE EMPLOYEES
 (
-	emp_ssn int not null
-	work_phone int
-	salary int
-	fname varchar(30) not null
-	lname varchar(30) not null
-	broker_company varchar(50)
+	emp_ssn int not null,
+	work_phone int,
+	salary int,
+	fname varchar(30) not null,
+	lname varchar(30) not null,
+	broker_company varchar(50),
 	PRIMARY KEY(emp_ssn)
 )
