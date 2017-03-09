3f5a07db7272ada0bab6595db0203f2ec2bf8c3b
Dylan Brown <Dylan Brown>
2017-03-09 13:43:49 -0500

    employees table script

---
+++ createInsurance.sql
@@ -0,0 +1,12 @@
+use insurance;
+
+CREATE TABLE EMPLOYEES
+(
+	emp_ssn int not null
+	work_phone int
+	salary int
+	fname varchar(30) not null
+	lname varchar(30) not null
+	broker_company varchar(50)
+	PRIMARY KEY(emp_ssn)
+)
